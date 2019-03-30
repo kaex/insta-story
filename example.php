564e5badb2c6b67e3b17d7cd998195f89d89f2da
Baran <48059103+xosad@users.noreply.github.com>
2019-03-30 15:41:47 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,25 +1,13 @@
 <?php
-include 'func.php';
+require_once 'class.instagram.php';
 if (isset($_POST['submit'])) {
-    $username = $_POST['username'];
-    $stories = GetAllStories($username);
-    foreach ($stories['story'] as $story) {
-        $check = '/mp4/m';
-        preg_match_all($check, $story, $matches, PREG_SET_ORDER, 0);
-        if (empty($matches)) {
-            echo "<a href=$story&dl=1><img src=$story></a>";
-        } else {
-            echo '<video width="320" height="240" controls>';
-            echo '<source src="' . $story . '" type="video/mp4">';
-            echo '</video>';
-            echo "<a href=$story&dl=1>Download</a>";
-        }
-    }
+    $story = new instagram_story();
+    $story->getStory($_POST['username']);
 }
 ?>
 <div id="center" style="text-align: center; margin-top: 300px;">
-<form action="" method="post">
-    <input name="username">
-    <input type="submit" name="submit">
-</form>
+    <form action="" method="post">
+        <input name="username">
+        <input type="submit" name="submit">
+    </form>
 </div>
